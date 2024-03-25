556dd2c98f3655545ec3489479b88c59143d351e
IbnKamil <163489394+IbnKamil@users.noreply.github.com>
2024-03-25 05:06:26 +0300

    Add files via upload

---
+++ Operator_for.py
@@ -0,0 +1,7 @@
+car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in car:
+    print('Я езжу на автомабиле марки', i)
+cars_count = 0
+for i in range(len(car)):
+    cars_count += i + 10
+    print(cars_count)
